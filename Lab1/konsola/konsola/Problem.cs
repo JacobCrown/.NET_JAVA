7776efefbdfaf91e2b12a49b2c6da57d01b14b30
Jacob Crown <jkorona.pl@gmail.com>
2022-03-14 16:37:11 +0100

    initial commit

---
+++ Lab1/konsola/konsola/Problem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsola
+{
+    class Problem
+    {
+        public int n;
+        public int seed;
+        public int c;
+        public int[] v_list;
+        public int[] w_list;
+
+        public Problem(int number, int seed_num, int capacity)
+        {
+            n = number;
+            seed = seed_num;
+            c = capacity;
+            v_list = new int[n];
+            w_list = new int[n];
+        }
+
+        public void solve_problem()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "Hello There!";
+        }
+    }
+}
---
+++ Lab1/konsola/konsola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę n:");
+            int n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Podaj seed:");
+            int seed = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(seed);
+            Console.WriteLine("Podaj pojemność plecaka:");
+            int c = Convert.ToInt16(Console.ReadLine());
+            Problem p = new Problem(n, seed, c);
+
+
+            Console.WriteLine(p.ToString());
+
+        }
+    }
+}
