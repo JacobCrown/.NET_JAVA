85c31dfa57edeef93648a09792db1a2cdf1d8cc1
Jacob Crown <jkorona.pl@gmail.com>
2022-03-22 14:14:43 +0100

    add test for problem

---
+++ Lab1/konsola/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("81fd49c9-ea5e-4188-a8e8-775694a6f085")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lab1/konsola/UnitTest/UnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using konsola;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void help_method(Problem p)
+        {
+            p.generateSeedValues();
+            p.items.Sort(delegate (Item x, Item y)
+            {
+                return (y.w * y.val).CompareTo(x.w * x.val);
+            });
+            p.solveProblem();
+        }
+        [TestMethod]
+        public void test_many_seed_values_for_empty_capacity()
+        {
+            for (int seed = 0; seed < 100; seed++)
+            {
+                Problem p = new Problem(12, seed, 0);
+                help_method(p);
+                Assert.AreEqual(12, p.items.Count);
+                Assert.AreEqual(0, p.plecak.Count);
+            }
+        }
+
+        [TestMethod]
+        public void test_string_repr_of_item()
+        {
+            Item p = new Item(1, 12, 12);
+            Assert.AreEqual("1-> Value: 12 | Weight: 12 | v*w: 144", p.ToString());
+        }
+
+        [TestMethod]
+        public void test_backpack_not_empty()
+        {
+            for (int seed = 0; seed < 100; seed++)
+            {
+                Problem p = new Problem(12, seed, 100);
+                help_method(p);
+                Assert.AreNotEqual(0, p.plecak.Count);
+            }
+        }
+
+        [TestMethod]
+        public void test_all_items_in_backpack()
+        {
+            for (int seed = 0; seed < 100; seed++)
+            {
+                Problem p = new Problem(4, seed, 100);
+                help_method(p);
+                Assert.AreEqual(4, p.plecak.Count);
+            }
+        }
+
+        [TestMethod]
+        public void test_string_repr_of_problem()
+        {
+            Problem p = new Problem(4, 12, 10);
+            help_method(p);
+            string s = "";
+            foreach (Item item in p.items)
+            {
+                s += item.ToString();
+                s += "\r\n";
+            }
+            Assert.AreEqual(s, p.ToString());
+        }
+
+        [TestMethod]
+        public void test_algorithm_doesnt_cheat()
+        {
+            Problem p = new Problem(100, 12, 200);
+            help_method(p);
+            foreach(Item item in p.items)
+            {
+                if(!(p.plecak.Contains(item)))
+                {
+                    Assert.IsTrue(p.c < item.w);
+                }
+            }
+        }
+    }
+}
